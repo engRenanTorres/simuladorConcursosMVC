5ab8a4cd466cdd3e42539051a2c70bdf52f12b1c
RenanWindowsMesa <engrtorres@outlook.com>
2022-10-06 19:46:17 -0300

    Initial commit

---
+++ simuladorconcurso/src/main/kotlin/com/renantorres/simuladorconcurso/SimuladorconcursoApplication.kt
@@ -0,0 +1,11 @@
+package com.renantorres.simuladorconcurso
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimuladorconcursoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimuladorconcursoApplication>(*args)
+}
---
+++ simuladorconcurso/src/test/kotlin/com/renantorres/simuladorconcurso/SimuladorconcursoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.renantorres.simuladorconcurso
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimuladorconcursoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
